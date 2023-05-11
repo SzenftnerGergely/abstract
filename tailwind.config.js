be0e91dcfb421ef9a5db78bb6b08ae0a1e24442e
SzenftnerGergely <gergely.szenftner@gmail.com>
2023-05-11 13:44:44 +0200

    initial

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        blue: "#4c5fd5",
+        grey: "#191a1b",
+        light_grey: "#dadbf1",
+    },
+    },
+  },
+  plugins: [],
+}
